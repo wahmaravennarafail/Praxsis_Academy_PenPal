310f89272ea040cdf66608aefc86c9e70bfc904b
yasinahmad12 <yasin.inf18@student.unu-jogja.ac.id>
2021-10-25 15:37:57 +0700

    tampilan login

--- penpal/task/admin.py
+++ penpal/task/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+@admin.register(models.login)
+class PersonAdmin(admin.ModelAdmin):
+    pass
--- penpal/task/models.py
+++ penpal/task/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-<<<<<<< HEAD
 
 class pesan(models.Model):
     pesan = models.TextField()
     negara = models.CharField(max_length=20)
     initial = models.CharField(max_length=10)
-=======
-# Create your models here.
+
+class login(models.Model):
+    email= models.CharField(max_length=20)
+    password = models.CharField(max_length=8)
+
     
->>>>>>> 61169c6a0b4354b11597e961c624cf4a8dce8a99
+
+# Create your models here.
