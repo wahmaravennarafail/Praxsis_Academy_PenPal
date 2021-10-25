6aa28b181d208794beb4f15352c1f5c5723a0f25
Rizal Hidayat <rizalhidayat006@gmail.com>
2021-10-25 11:31:15 +0700

    Edit Form untuk input message

--- penpal/task/models.py
+++ penpal/task/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from django.db.models.base import Model
 
-<<<<<<< HEAD
 
-class pesan(models.Model):
+class message(models.Model):
     pesan = models.TextField()
     negara = models.CharField(max_length=20)
     initial = models.CharField(max_length=10)
-=======
-# Create your models here.
-    
->>>>>>> 61169c6a0b4354b11597e961c624cf4a8dce8a99
