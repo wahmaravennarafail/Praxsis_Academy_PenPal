3c585cb8d37d4cb9f0c558091bd8dfa6667a7c6a
wahmaravennarafail <wahmaravenna78@gmail.com>
2021-10-25 16:40:40 +0700

    membuat tampilan

--- penpal/task/urls.py
+++ penpal/task/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from . views import *
 from penpal.task import views
+
 urlpatterns = [
     path('', home, name="home"),
     path('login', login, name='login'),
     path('pesan', pesan, name='pesan'),
-    path('profile/', views.profile),
+
 
 
 ]
